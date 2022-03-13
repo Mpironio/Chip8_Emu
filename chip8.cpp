3857e5980c69db3159b06e1bc6252ff4e8975ea5
Mpironio <mpironio97@gmail.com>
2022-03-13 03:36:15 -0300

    a few more instructions

--- chip8.cpp
+++ chip8.cpp
@@ -65,6 +65,9 @@ void chip8::emulateCycle() {
 	_opcode = _memory[_pc] << 8 | _memory[_pc + 1];
 
 	//Decode
+	WORD Vx = _regs[(_opcode >> 8) & 0xF];
+	WORD Vy = _regs[(_opcode >> 4) & 0xF];
+	WORD Vf = _regs[15];
 	switch (_opcode & 0xF000) {
 	case 0x0000:
 		switch (_opcode & 0x000F) {
@@ -91,51 +94,137 @@ void chip8::emulateCycle() {
 		break;
 
 	case 0x3000:	//Caso 3xkk: SE Vx, byte
-		_regs[(_opcode >> 8) & 0xF] == _opcode & 0x00FF ? _pc += 4 : _pc = _pc;
+		Vx == _opcode & 0x00FF ? _pc += 4 : _pc = _pc + 2;
 		break;
 
 	case 0x4000:	//Caso 4xkk: SNE Vx, byte
-		_regs[(_opcode >> 8) & 0xF] != _opcode & 0x00FF ? _pc += 4 : _pc = _pc;
+		_regs[(_opcode >> 8) & 0xF] != _opcode & 0x00FF ? _pc += 4 : _pc = _pc + 2;
 		break;
 
 	case 0x5000:	//Caso 5xy0: SE Vx, Vy
-		_regs[(_opcode >> 8) & 0xF] == _regs[(_opcode >> 4) & 0xF] ? _pc += 4 : _pc = _pc;
+		Vx == Vy ? _pc += 4 : _pc = _pc + 2;
 		break;
 
 	case 0x6000:	//Caso 6xkk: LD Vx, byte
-		_regs[(_opcode >> 8) & 0xF] = _opcode & 0xFF;
+		Vx = _opcode & 0xFF;
+		_pc += 2;
 		break;
 
 	case 0x7000:	//Caso 7xkk: ADD Vx, byte
-		_regs[(_opcode >> 8) & 0xF] += _opcode & 0xFF;
+		Vx += _opcode & 0xFF;
+		_pc += 2;
 		break;
 
 	case 0x8000:	
 		switch (_opcode & 0xF) {
 		case 0x0:	//Caso 8xy0: LD Vx, Vy
-			_regs[(_opcode >> 8) & 0xF] = _regs[(_opcode >> 4) & 0xF];
+			Vx = Vy;
+			_pc += 2;
 			break;
 		case 0x1:	//Caso 8xy1: OR Vx, Vy
-			_regs[(_opcode >> 8) & 0xF] = _regs[(_opcode >> 8) & 0xF] | _regs[(_opcode >> 4) & 0xF];
+			Vx = Vx | Vy;
+			_pc += 2;
 			break;
 		case 0x2:	//Caso 8xy2: AND Vx, Vy
-			_regs[(_opcode >> 8) & 0xF] = _regs[(_opcode >> 8) & 0xF] & _regs[(_opcode >> 4) & 0xF];
+			Vx = Vx & Vy;
+			_pc += 2;
 			break;
 		case 0x3:	//Caso 8xy3: XOR Vx, Vy
-			_regs[(_opcode >> 8) & 0xF] = _regs[(_opcode >> 8) & 0xF] ^ _regs[(_opcode >> 4) & 0xF];
+			Vx = Vx ^ Vy;
+			_pc += 2;
 			break;
 		case 0x4:	//Caso 8xy4: ADD Vx, Vy
-			WORD Vx = _regs[(_opcode >> 8) & 0xF];
-			WORD sum = _regs[(_opcode >> 8) & 0xF] + _regs[(_opcode >> 4) & 0xF];
-			sum > 255 ? Vx = sum, _regs[15] = 1 : _regs[15] = 0;
+			WORD sum = Vx + Vy;
+			sum > 255 ? Vx = sum, Vf = 1 : Vf = 0;
+			_pc += 2;
+			break;
+		case 0x5:	//Caso 8xy5: SUB Vx, Vy
+			Vx > Vy ? Vf = 1: Vf = 0;
+			Vx = Vx - Vy;
+			_pc += 2;
+			break;
+		case 0x6:	//Caso 8xy6: SHR Vx {, Vy}
+			Vx & 0xF == 1 ? Vf = 1 : Vf = 0;
+			Vx /= 2;
+			_pc += 2;
+			break;
+		case 0x7:	//Caso 8xy7: SUBN Vx, Vy
+			Vy > Vx ? Vf = 1 : Vf = 0;
+			Vx = Vy - Vx;
+			_pc += 2;
+			break;
+		case 0xE:	//Caso 8xyE: SHL Vx {, Vy}
+			Vx & 0xF == 1 ? Vf = 1 : Vf = 0;
+			Vx *= 2;
+			_pc += 2;
 			break;
 
-		case 0x5:	//Caso 8xy5: SUB Vx, Vy
+
+		}
+	case 0x9000:	//Caso 9xy0: SNE Vx, Vy
+		Vx != Vy ? _pc += 4 : _pc = _pc + 2;
+		break;
+
+	case 0xA000:	//Caso Annn: LD I, addr
+		_I = _opcode & 0x0FFF;
+		_pc += 2;
+		break;
+	
+	case 0xB000:	//Caso Bnnn: JP V0, addr
+		_pc = (_opcode & 0xFFF) + _regs[0];
+		break;
+
+	case 0xC000:	//Caso Cxkk: RND Vx, byte
+		// Vx = número random ( 0 - 255 ) & kk
+		_pc += 2;
+		break;
+
+	case 0xD000:	//Caso Dxyn: DRW Vx, Vy, nibble
+		_pc += 2;
+		break;
+	
+	case 0xE000:
+		switch (_opcode & 0xF) {
+		case 0x000E: //Caso Ex9E: SKP Vx
+			_key[_regs[(_opcode & 0xF00) >> 8]] != 0 ? _pc += 4 : _pc += 2;
+			break;
+		case 0x0001: //Caso ExA1: SKNP Vx
+			_key[_regs[(_opcode & 0xF00) >> 8]] == 0 ? _pc += 4 : _pc += 2;
+			break;
+		}
+
+	case 0xF000:
+		switch (_opcode & 0xFF) {
+		case 0x0007: //Caso Fx07: LD Vx, DT
+			Vx = _delayTimer;
+			_pc += 2;
+			break;
+		case 0x000A: //Caso Fx0A: LD Vx, K
+			WORD keyNumber = (_opcode & 0xF00) >> 8;
+			_key[keyNumber] ? _regs[Vx] = keyNumber, _pc += 2 : _pc -= 2; //la forma de que una instrucción se repita hasta que se haga lo pedido es que el pc vuelva al lugar donde empezaba esa instrucción
+			break;
+		case 0x0015: //Caso Fx15: LD DT, Vx
+			_delayTimer = Vx;
+			_pc += 2;
+			break;
+		case 0x0018: //Caso Fx18: LD ST, Vx
+			_soundTimer = Vx;
+			_pc += 2;
+			break;
+		case 0x001E: //Caso Fx1E: ADD I, Vx
+			_I += Vx;
+			_pc += 2;
+			break;
+		case 0x0029: //Caso Fx29: LD F, Vx	//Se guarda en _I la posición de memoria donde se encuentra el carácter almacenado en Vx. El fontset arranca en 0x50
+					 //Cada caracter se representa con 5 líneas de 1 byte, por lo que tendremos un caracter distinto cada 5 bytes
+			WORD digit = _regs[Vx];
+			_I = FONTSET_START_ADRESS + (5 * digit);
+			_pc += 2;
+			break;
+		case 0x0033: //Caso Fx33: LD B, Vx
 
 		}
 		
-
-	
 	}
 
 
